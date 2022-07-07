7e6b815d4e6ad4c0172c18b6a65fbb4f3bb18d58
HP <HP@DESKTOP-3O1JLKM>
2022-07-07 16:42:26 +0530

    Build01 TC

---
+++ Hybrideprojectprgs/src/generic/Frame_constants.java
@@ -0,0 +1,12 @@
+package generic;
+
+public interface Frame_constants {
+		String gecko_key = "webdriver.gecko.driver";
+		String gecko_value = "./softwares/geckodriver.exe";
+		
+		String chrome_key = "webdriver.chrome.driver";
+		String chrome_value = "./softwares/chromedriver.exe";
+		
+		String base_url = "https://www.facebook.com";
+
+}
